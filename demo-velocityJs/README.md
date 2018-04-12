36c9b887aceb03022f60477640d9e77c0e64e49e
Jose Garcia <josegarciamanez@gmail.com>
2018-04-12 12:10:41 +0200

    cambios

--- demo-velocityJs/README.md
+++
@@ -1,2 +0,0 @@
-# TRABAJO CON SKELETON.CSS Y VELOCITY.JS
-### Pruebas con varias librerías.
---
+++ practicas/eloquent/app.js
@@ -0,0 +1,4 @@
+// Ejercicio 1 Tema 2
+// for (let simb = "#"; simb.length < 8; simb += '#') {
+//     console.log(simb);
+// }
